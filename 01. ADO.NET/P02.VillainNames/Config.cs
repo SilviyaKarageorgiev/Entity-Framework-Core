1e3015f3baa21b5d15421ff1e40b33f4d3f31bb2
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-15 21:50:51 +0200

    Add

--- 01. ADO.NET/P02.VillainNames/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P02.VillainNames
-{
-    public static class Config
-    {
-        public const string ConnectionString = @"Server=DESKTOP-OOL5FKL\SQLEXPRESS;Database=MinionsDB;Integrated Security=True";
-    }
-}
