526de9a694350466dcbed19cdfd0a6ca216f700f
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-23 23:59:23 +0200

    Update SqlQueries.cs

--- 01. ADO.NET/ADO.NET/SqlQueries.cs
+++ 01. ADO.NET/ADO.NET/SqlQueries.cs
@@ -14,13 +14,12 @@
 
         public const string GetAllMinionsByVillainId = 
             @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) AS RowNum,
-                                         m.Name, 
-                                         m.Age
-                                    FROM MinionsVillains AS mv
-                                    JOIN Minions As m ON mv.MinionId =                          m.Id
-                                   WHERE mv.VillainId = @Id
-                                ORDER BY m.Name";
+                     m.Name, 
+                     m.Age
+                FROM MinionsVillains AS mv
+                JOIN Minions As m ON mv.MinionId = m.Id
+               WHERE mv.VillainId = @Id
+            ORDER BY m.Name";
     }
 
-    
 }
