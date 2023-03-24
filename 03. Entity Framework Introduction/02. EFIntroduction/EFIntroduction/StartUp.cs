f6ea77e35900d24a729563e568828cfe95599756
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-24 15:32:15 +0200

    Add

---
+++ 03. Entity Framework Introduction/02. EFIntroduction/EFIntroduction/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        //SoftUniContext dbContext = new SoftUniContext();
+    }
+}
