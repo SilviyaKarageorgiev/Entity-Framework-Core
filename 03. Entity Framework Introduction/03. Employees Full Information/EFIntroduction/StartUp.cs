6fb22a756baf70b74a5a7ad962487c3426403ca3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-24 19:31:44 +0200

    Add

---
+++ 03. Entity Framework Introduction/03. Employees Full Information/EFIntroduction/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+
+        string result = GetEmployeesFullInformation(dbContext);
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Employee[] employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .ToArray();
+
+        foreach (Employee e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
