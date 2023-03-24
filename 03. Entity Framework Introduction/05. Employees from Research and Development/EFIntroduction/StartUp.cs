bddd8665c106aaf609eedf49bab70eb7d0b7f508
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-25 00:05:44 +0200

    Add

---
+++ 03. Entity Framework Introduction/05. Employees from Research and Development/EFIntroduction/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+
+        string result = GetEmployeesFromResearchAndDevelopment(dbContext);
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                DepartmentName = e.Department.Name,
+                e.Salary
+            })
+            .OrderBy(e => e.Salary)
+            .ThenByDescending(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
