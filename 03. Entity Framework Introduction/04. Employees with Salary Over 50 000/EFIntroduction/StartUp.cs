ad99f8c36f14e8f4dbad894ed54557dda1c63104
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-24 23:46:55 +0200

    Add

---
+++ 03. Entity Framework Introduction/04. Employees with Salary Over 50 000/EFIntroduction/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+
+        string result = GetEmployeesWithSalaryOver50000(dbContext);
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Salary > 50000)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .OrderBy(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} - {e.Salary:F2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
