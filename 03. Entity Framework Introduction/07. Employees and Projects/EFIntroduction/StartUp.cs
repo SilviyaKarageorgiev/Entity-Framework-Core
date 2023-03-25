5274e623c117c0dcd831b562ba5bc47c4df94e1c
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-25 14:37:55 +0200

    Add

---
+++ 03. Entity Framework Introduction/07. Employees and Projects/EFIntroduction/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+
+        string result = GetEmployeesInPeriod(dbContext);
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesInPeriod(SoftUniContext context)
+    {
+
+        StringBuilder sb = new StringBuilder();
+
+        var employeesWithProjects = context.Employees
+            .Take(10)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                ManagerFirstName = e.Manager!.FirstName,
+                ManagerLastName = e.Manager!.LastName,
+                Projects = e.EmployeesProjects
+                    .Where(ep => ep.Project.StartDate.Year >= 2001 &&
+                                 ep.Project.StartDate.Year <= 2003)
+                    .Select(ep => new
+                    {
+                        ProjecName = ep.Project.Name,
+                        StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                        EndDate = ep.Project.EndDate.HasValue ?
+                                  ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture) : "not finished"
+                    })
+                    .ToArray()
+            })
+            .ToArray();
+
+        foreach (var e in employeesWithProjects)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+
+            foreach (var p in e.Projects)
+            {
+                sb.AppendLine($"--{p.ProjecName} - {p.StartDate} - {p.EndDate}");
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
