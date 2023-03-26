c4e6301687bda7c8fdef567d8bc5857b6b5129c9
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-03-26 23:48:51 +0300

    Add

---
+++ 04. Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Enums/Prediction.cs
@@ -0,0 +1,12 @@
+﻿namespace P02_FootballBetting.Data.Models.Enums
+{
+    // Enum are not entities in the DB
+    // Enum are string representation of int values
+    // In the DB -> int
+    public enum Prediction
+    {
+        Draw = 0,
+        Win = 1,
+        Lose = 2
+    }
+}
