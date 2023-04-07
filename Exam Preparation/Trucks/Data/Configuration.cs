d01bc206e75cf1634dbfb4f845d968ef9aabd55b
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-04-08 00:55:42 +0300

    Add

---
+++ Exam Preparation/Trucks/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-UNCF2AK\SQLEXPRESS01;Database=Trucks;Trusted_Connection=True";
+    }
+}
