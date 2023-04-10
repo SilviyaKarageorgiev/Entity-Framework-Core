01b2a5d447e7e8ab25dbc6030627e5073f1593d0
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-04-11 00:17:51 +0300

    Add

---
+++ Exam Preparation/Artillery/Artillery/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Artillery.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-UNCF2AK\SQLEXPRESS01;Database=Artillery;Integrated Security=True;Encrypt=False";
+    }
+}
